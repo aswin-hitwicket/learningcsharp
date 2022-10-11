daad1009e8f9d916b74248291255ef81cce33ec5
Aswin Murali <aswin.murali2019@vitstudent.ac.in>
2022-10-11 11:40:17 +0530

    Add project files.

---
+++ LearnHitwicket/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Hitwicket
+{
+    public class Hitwicket
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + ", ");
+                }
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+
+            Console.Write("Your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is " + age);
+
+            Random random = new Random();
+
+            var randomAdder = random.Next(1, 5);
+            Console.WriteLine(randomAdder);
+
+            age = randomAdder + age;
+            Console.WriteLine("Your random age is " + age);
+
+            if (age > 18)
+            {
+                Console.WriteLine("Eligible!");
+            }
+            else
+            {
+                Console.WriteLine("Not Eligible!");
+            }
+
+            switch (age)
+            {
+                case 0:
+                    Console.WriteLine(0);
+                    break;
+                case 1:
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

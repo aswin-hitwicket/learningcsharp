c00587644b0dcbf048b09cdb006b0f6ac9816a28
Aswin Murali <aswin.murali2019@vitstudent.ac.in>
2022-10-11 14:11:53 +0530

    learns constructor in obj program

--- LearnHitwicket/ObjectProgram.cs
+++ LearnHitwicket/ObjectProgram.cs
@@ -3,16 +3,24 @@
     class Animal
     {
         int age;
-        string name = "hiii";
+        string name;
+
+        public Animal(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
 
         public string GetName() { return name; }
+        public int GetAge() { return age; }
     }
     internal class ObjectProgram
     {
         public static void AnimalProgram()
         {
-            var obj = new Animal();
+            var obj = new Animal(1, "f");
             Console.WriteLine(obj.GetName());
+            Console.WriteLine(obj.GetAge());
         }
     }
 }

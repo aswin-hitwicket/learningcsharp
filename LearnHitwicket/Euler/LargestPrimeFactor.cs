49e68c1f601838ce9ccd7950e1d251340704735b
Aswin Murali <aswin.murali2019@vitstudent.ac.in>
2022-10-13 17:55:15 +0530

    adds Largest Prime Factor program

---
+++ LearnHitwicket/Euler/LargestPrimeFactor.cs
@@ -0,0 +1,20 @@
+﻿namespace LearnHitwicket.Euler
+{
+    internal class LargestPrimeFactor
+    {
+        public static void LargestPrimeFactorProgram()
+        {
+            long num = 13195;
+            string answer = "";
+
+            for(int i = 1; i < num; i++)
+            {
+                if (num % i == 0 && num % 1 == 0)
+                {
+                    answer += i + ", ";
+                }
+            }
+            Console.WriteLine("The prime factor(s) include " + answer);
+        }
+    }
+}

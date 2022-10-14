66cecca28735697c60cebb53295fdd99facdb724
Aswin Murali <aswin.murali2019@vitstudent.ac.in>
2022-10-14 17:16:58 +0530

    adds Self Power program

---
+++ LearnHitwicket/Euler/SelfPower.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+namespace LearnHitwicket.Euler
+{
+    internal class SelfPower
+    {
+        public static void SelfPowerMain()
+        {
+            var sum = new BigInteger();
+            for (int i = 1; i <= 1000; i++)
+            {
+                sum += BigInteger.Pow(i, i);
+            }
+
+            var sumString = sum.ToString();
+            var lastTenDigits = sumString.Substring(Math.Max(0, sumString.Length - 10));
+
+            Console.WriteLine(lastTenDigits);
+        }
+    }
+}
